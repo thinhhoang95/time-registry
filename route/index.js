9f4a690db295620ca0da2618eb6bf247e6c32372
Hoang Dinh Thinh <hoangdinhthinh@live.com>
2019-10-01 13:36:16 +0700

    Report

--- route/index.js
+++ route/index.js
@@ -14,11 +14,98 @@ router.post('/registry', async (req, res) => {
         cardId: cardId
     });
     await registry.save();
-    await mailer(cardId);
+    let report = await queryReport();
+    await mailer(cardId, report);
     res.status(200).send("OK");
 });
 
-const mailer = async (cardId) => {
+let queryReport = async() => {
+    let returnStr = '';
+    let fromDate = new Date();
+    fromDate.setHours(0);
+    fromDate.setMinutes(0);
+    fromDate.setMilliseconds(0);
+    let toDate = new Date();
+    toDate.setHours(0);
+    toDate.setMinutes(0);
+    toDate.setMilliseconds(0);
+    toDate.setDate(toDate.getDate() + 1);
+    let aggregateRules = [
+        {
+          '$match': {
+            'date': {
+              '$gte': fromDate
+            }, 
+            'date': {
+              '$lte': toDate
+            }
+          }
+        }, {
+          '$lookup': {
+            'from': 'subjects', 
+            'localField': 'cardId', 
+            'foreignField': 'cardId', 
+            'as': 'subject'
+          }
+        }, {
+          '$project': {
+            '_id': '$_id', 
+            'cardId': '$cardId', 
+            'date': '$date', 
+            'subject': {
+              '$arrayElemAt': [
+                '$subject', 0
+              ]
+            }
+          }
+        }, {
+          '$sort': {
+            'date': 1
+          }
+        }, {
+          '$group': {
+            '_id': '$cardId', 
+            'subjectName': {
+              '$first': '$subject.subjectName'
+            }, 
+            'description': {
+              '$first': '$subject.description'
+            }, 
+            'time': {
+              '$push': '$date'
+            }
+          }
+        }
+      ];
+    let registries = await Registry.aggregate(aggregateRules);
+    registries.forEach((registry) => {
+        returnStr += '-----\n';
+        returnStr += 'SUBJECT: ' + registry.subjectName + '\n';
+        returnStr += 'DESCRIPTION: ' + registry.description + '\n';
+        let timeEntries = registry.time;
+        let ongoing = false;
+        let prevTime;
+        let timeAccumulated = 0;
+        let minutesAccumulated = 0;
+        timeEntries.forEach((time) => {
+            ongoing = !ongoing;
+            if (ongoing)
+            {
+                prevTime = time;
+            } else 
+            {
+                timeAccumulated += Math.abs(time-prevTime);
+            }
+        });
+        console.log(timeAccumulated);
+        minutesAccumulated = Math.ceil(timeAccumulated / (1000 * 60));
+        returnStr += "ACCUMULATED: " + Number(minutesAccumulated).toFixed(2) + " minutes. \n";
+        returnStr += "STATUS: " + (ongoing?"Yes":"No") + "\n";
+    });
+    return returnStr;
+}
+
+const mailer = async (cardId, report) => {
     let sj = await Subject.findOne({cardId: cardId});
     let subject = sj.subjectName;
     let description = sj.description;
@@ -34,7 +121,7 @@ const mailer = async (cardId) => {
         from: 'thinhhoangdinh95@hotmail.com', // Sender address
         to: 'hoangdinhthinh@live.com',         // List of recipients
         subject: 'New task event registered', // Subject line
-        text: 'Hello Thinh. \n A new task has been registered with the following information: \n Card ID: ' + cardId + ' \n Subject: ' + subject + '\n Description: ' + description + '\n' // Plain text body
+        text: 'Hello Thinh. \n A new task has been registered with the following information: \n Card ID: ' + cardId + ' \n Subject: ' + subject + '\n Description: ' + description + '\n' + report // Plain text body
     };
     transport.sendMail(message, function (err, info) {
         if (err) {
